e9dab16470aab312b42b05538a83573a494d3101
Abhi-2116 <abhibadal2116@gmail.com>
2026-07-03 22:53:54 +0530

    css project

---
+++ name.js
@@ -0,0 +1,24 @@
+console.log("I am Abhi")
+var a=23;
+console.log(a);
+
+a=45;
+console.log(a);
+
+var a=56;
+console.log(a);
+
+let ab=45
+console.log(ab)
+
+ab=56
+console.log(ab)
+
+const abb=67
+console.log(abb)
+
+// abb=7890
+// console.log(abb)
+
+
+
